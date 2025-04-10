be09612497fcefe6b5915193f232d3deec6c76a0
isluel <isluel@nate.com>
2025-04-10 22:34:36 +0900

    initial commit

---
+++ src/test/java/com/isluel/librarysearch/LibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isluel.librarysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
