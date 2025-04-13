a780fec94a139af36ab660ab173ff41360e8b8ea
isluel <isluel@nate.com>
2025-04-13 18:55:33 +0900

    [LS-8] 문서화 적용

--- search-api/src/main/java/com/library/controller/response/SearchResponse.java
+++ search-api/src/main/java/com/library/controller/response/SearchResponse.java
@@ -1,10 +1,22 @@
 package com.library.controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Builder
-public record SearchResponse(String title, String author, String publisher, LocalDate pubDate, String isbn) {
+@Schema(description = "검색 결과")
+public record SearchResponse(
+        @Schema(description = "제목", example = "HTTP완벽가이드")
+        String title,
+        @Schema(description = "작가", example = "데이빗고울리")
+        String author,
+        @Schema(description = "출판사", example = "인사이트")
+        String publisher,
+        @Schema(description = "출판일", example = "2015-01-01")
+        LocalDate pubDate,
+        @Schema(description = "isbn", example = "9788966261208")
+        String isbn) {
 }
