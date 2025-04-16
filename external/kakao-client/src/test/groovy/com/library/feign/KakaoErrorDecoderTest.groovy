37a620d52e53ec33a9313c0c1e0905b5683fa2ef
isluel <isluel@nate.com>
2025-04-16 21:01:03 +0900

    [LS-9] 코드 정리

--- external/kakao-client/src/test/groovy/com/library/feign/KakaoErrorDecoderTest.groovy
+++ external/kakao-client/src/test/groovy/com/library/feign/KakaoErrorDecoderTest.groovy
@@ -1,12 +1,11 @@
 package com.library.feign
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.library.ApiException
-import com.library.ErrorType
+import com.library.exception.ApiException
+import com.library.exception.ErrorType
 import com.library.KakaoErrorResponse
 import feign.Request
 import feign.Response
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import spock.lang.Specification
 
